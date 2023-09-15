ecd79f6589f4e4eab2b626b3b1882ae18b7c0eb0
trisha-chowdhury <113051031+trisha-chowdhury@users.noreply.github.com>
2023-09-15 23:28:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,87 @@
-const accesskey="xU63CgNDAJ6PB2cb2s4rsgF_aiUYdyqDS_JhY7N7vdE";
-const searchForm=document.getElementById("search-form");
-const searchInput=document.getElementById("search-input");
-const searchResult=document.getElementById("search-result");
-const showMore=document.getElementById("show-more-button");
+const imageWrapper=document.querySelector(".images");
+const searchInput=document.querySelector(".search input")
+const showMore=document.querySelector(".gallery .load");
+const lightbox=document.querySelector(".lightbox");
+const downloadImagebutton=document.querySelector(".uil-import");
+const closeimgbutton=document.querySelector(".close-icon");
 
-let keyword="";
-let page=1;
+const apikey="soXbrn6WhVstQmDZUEN48g1NYRbJ7YsMkIMM06xDPglA4aqYk1aZWk0i";
+const perpage=11;
+let currentpage=1;
+let searchTerm=null;
 
-async function searchImages(){
-    keyword=searchInput.value;
-    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
-
-    const response=await fetch(url);
-    const data=await response.json();
-    if(page===1){
-        searchResult.innerHTML="";
-    }
-
-    const results=data.results;
-    results.map((result)=>{
-        const image=document.createElement("img");
-        image.src=result.urls.small;
-        const imageLink=document.createElement("a");
-        imageLink.href=result.links.html;
-        imageLink.target="_blank";
-
-        imageLink.appendChild(image);
-        searchResult.appendChild(imageLink);
-    })
-    showMore.style.display="block";
+const downloadImage=(imgURL)=>{
+    //converting the received image to blob and creating its download link to download it
+    fetch(imgURL).then(res=>res.blob()).then(blob=>{
+        const a=document.createElement("a");
+        a.href=URL.createObjectURL(blob);
+        a.download=new Date().getTime();
+        a.click();
+    }).catch(()=>alert("Failed to download images!"));
+}
+const showlightbox=(name,img)=>{
+    lightbox.querySelector("img").src=img;
+    lightbox.querySelector("span").innerText=name;
+    downloadImagebutton.setAttribute("data-img",img);
+    lightbox.classList.add("show");
+    document.body.style.overflow="hidden";
 }
 
-searchForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    page=1;
-    searchImages();
-})
+const hidelightbox=()=>{
+    lightbox.classList.remove("show");
+    document.body.style.overflow="auto";
+}
 
-showMore.addEventListener("click",()=>{
-    page++;
-    searchImages();
-})
+const generateHTML=(images)=>{
+    imageWrapper.innerHTML+=images.map(img=>
+        `<li class="card">
+            <img onclick="showlightbox('${img.photographer}','${img.src.large2x}')" src="${img.src.large2x}" alt="img">
+                <div class="details">
+                    <div class="photographer">
+                        <i class="uil uil-camera"></i>
+                        <span>${img.photographer}</span>
+                    </div>
+                    <button onclick="downloadImage('${img.src.large2x}');">
+                        <i class="uil uil-import"></i>
+                    </button>
+                </div>
+            </img>
+        </li>`
+    ).join("");
+}
+
+const getImages = (apiURL) =>{
+    searchInput.blur();
+    showMore.innerText="Loading.....";
+    showMore.classList.add("disabled");
+    fetch(apiURL,{
+        headers:{Authorization:apikey}
+    }).then(res=>res.json()).then(data=>{
+        generateHTML(data.photos);
+        showMore.innerText="Show More";
+        showMore.classList.remove("disabled");
+    }).catch(()=>alert("Failed to download images"));
+}
+
+const showMoreImages=()=>{
+    currentpage++;
+    let apiURL=`https://api.pexels.com/v1/curated?page=${currentpage}&per_page=${perpage}`;
+    apiURL=searchTerm? `https://api.pexels.com/v1/search?query=${searchTerm}&per_page=${perpage}`:apiURL;
+    getImages(apiURL);
+}
+
+const showmoresearchImages=(e)=>{
+    if(e.target.value==="") return searchTerm=null;
+    if(e.key==="Enter"){
+    currentpage=1;
+    searchTerm=e.target.value;
+    imageWrapper.innerHTML="";
+    getImages(`https://api.pexels.com/v1/search?query=${searchTerm}$page=1&per_page=${perpage}`);
+}
+}
+
+getImages(`https://api.pexels.com/v1/curated?page=${currentpage}&per_page=${perpage}`);
+showMore.addEventListener("click",showMoreImages);
+searchInput.addEventListener("keyup",showmoresearchImages);
+closeimgbutton.addEventListener("click",hidelightbox);
+downloadImagebutton.addEventListener("click",(e)=>downloadImage(e.target.dataset.img));
